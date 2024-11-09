80fd4a7deffe55a78634fd2c2b4ec77eafdc5188
think-j <flame2080@naver.com>
2024-11-09 16:20:43 +0900

    Initial Commit

---
+++ pickerView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pickerView
+//
+//  Created by June 蘇 on 2024/11/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ pickerViewTests/pickerViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  pickerViewTests.swift
+//  pickerViewTests
+//
+//  Created by June 蘇 on 2024/11/09.
+//
+
+import Testing
+@testable import pickerView
+
+struct pickerViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
